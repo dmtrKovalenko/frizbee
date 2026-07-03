e4f9dba4c67352df79016f46ef02b991689d70ec
Liam Dyer <liamcdyer@gmail.com>
2026-07-02 23:39:07 -0400

    feat: simplify nix flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,28 @@
 {
-  inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, fenix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        fenixPkgs = fenix.packages.${system};
-
-        rustToolchain = fenixPkgs.minimal.toolchain;
-
-        # TODO: pick based on $system
-        x86_64_target = "x86_64-unknown-linux-gnu";
-        aarch64_target = "aarch64-unknown-linux-gnu";
-
-        toolchainWithTargets = fenixPkgs.combine [
-          rustToolchain
-          fenixPkgs.targets.${x86_64_target}.latest.rust-std
-          fenixPkgs.targets.${aarch64_target}.latest.rust-std
-        ];
-
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [ toolchainWithTargets ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            cargo
+            clippy
+            rustfmt
+            rust-analyzer
+          ];
         };
-      }
-    );
+      });
+    };
 }
